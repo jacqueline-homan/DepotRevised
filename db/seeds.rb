4f6f4bbfdabde86f7080e10c006671aa00ecadb1
Jacqueline S. Homan <jacquelinehoman7@gmail.com>
2015-08-10 15:29:03 -0400

    added initial CSS styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create!(title: 'Rails 3 Web Development',
+	description:
+	  %{<p>
+	  	Ruby on Rails is revolutionizing the Internet! If you need to
+	  	build a content management or a simple e-commerce app quickly,
+	  	Ruby and its framework, Rails, are must-haves for the 
+	  	programmers' toolbox.
+	  	</p>}, 
+	  	image_url: 'rails3.jpg',
+	  	price: 39.95)
